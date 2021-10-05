c552c75665eaaf96473f4ca9a211baf4d2aca3fd
zack0422 <84279315+zack0422@users.noreply.github.com>
2021-10-05 20:52:55 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ドロワー
+$(".drawer_icon").on("click", function (e) {
+  e.preventDefault();
+  $(".drawer_icon").addClass("is_active");
+  $(".drawer_icon_cross").addClass("is_active");
+  $(".drawer_content").addClass("is_active");
+});
+
+$(".drawer_icon_cross").on("click", function (e) {
+  e.preventDefault();
+  $(".drawer_icon").removeClass("is_active");
+  $(".drawer_icon_cross").removeClass("is_active");
+  $(".drawer_content").removeClass("is_active");
+});
+
+// アコーディオン
+$(".question_q").on("click", function () {
+  $(this).next().slideToggle();
+});
+
+// スムーススクロール
+$('a[href^="#"]').on("click", function () {
+  let header = $(".header").innerHeight();
+  let speed = 500;
+  let id = $(this).attr("href");
+  let target = $("#" == id ? "html" : id);
+  let position = $(target).offset().top - header;
+  $("html, body").animate({ scrollTop: position }, speed);
+});
